a6b396ab6118c031ea3b59cebc053a4799f40258
Josephine Rogers <jprogers017@gmail.com>
2019-04-13 20:01:46 -0700

    console annoucements

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const io = require('console-read-write');
 
 module.exports = async (client, message) => {
     const announcements = client.guilds.get(client.config.ids.server).channels.get(client.config.ids.announcements);
@@ -16,4 +17,34 @@ module.exports = async (client, message) => {
     }, 10000)
 
     console.log(`\n${client.user.tag} is online in ${client.guilds.size} server(s)!\n`);
+    // consoleAnnounce();
+
+    // async function consoleAnnounce() {
+    //     try {
+    //         function isReady() {
+    //             io.write('Are you ready to make an annoucement? Respond "Yes" or "No"!');
+    //             if (await io.read() === 'yes'.toLowerCase()) {
+    //                 io.write('Awaiting annoucement...');
+    //                 let annouce = await io.read();
+    //                 io.write(`Sending following annoucement to #annoucenments...\n${annouce}`);
+    //                 annouce = new Discord.RichEmbed()
+    //                     .setColor('73b6ff')
+    //                     .setDescription(`**${annouce}**`)
+    //                 announcements.send(annouce);
+    //                 // testing.send(annouce);
+    //             } else if (await io.read() === 'no'.toLowerCase()) {
+    //                 // function notReady() {
+    //                 //     io.write('Not right now? Okay! Let me know when you are ready to make one :)\n Just say "ready"!');
+    //                 //     await io.read() === 'ready'.toLowerCase()
+
+    //                 return;
+    //                 // }
+    //             } else {
+    //                 isReady();
+    //             }
+    //         }
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
 };
